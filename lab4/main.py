f9224404528c755ff014e247df9bdfdc7e2a6c8c
Danil Ginzburg <danilgin@inbox.ru>
2019-09-18 23:06:51 +0300

    fixed bugs

--- lab4/main.py
+++ lab4/main.py
@@ -16,7 +16,26 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
     
-    return render_template("welcome.html")
+    # return render_template("welcome.html")
+    return "\
+<html>\
+\
+<head>\
+	\
+	<link rel='shortcut icon' href='static/favicon.ico'>\
+\
+</head>\
+\
+<header> <title> My favourite meme </title> </header>\
+\
+<body> \
+\
+How many people have seen my meme: {}<br>\
+<img src='static/meme.jpg' />\
+</body>\
+\
+</html>\
+    ".format(visits)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
