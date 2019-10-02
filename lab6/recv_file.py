b6de6cb5faf3fe4db008f94984de6c96217c388a
Danil Ginzburg <danilgin@inbox.ru>
2019-10-02 20:33:32 +0300

    seems like working

--- lab6/recv_file.py
+++ lab6/recv_file.py
@@ -1,39 +1,55 @@
 import socket
 import os
 
-s = socket.socket()
-s.bind("localhost", 8000)
-s.listen(3)
+PORT = 8888
 
 
-def name_manager(file_name):
+def name_manager(file_name, dec=True):
     """
     Checks if this file name is already in use. In this case adds in the end of file _copy with number
+    :param dec: do we need to decode from bytes to string?
     :returns: updated file name
     """
+    if dec:
+        file_name = file_name.decode("utf-8")
+    file_name = file_name.replace("|", "")
     name, ext = file_name.split(".")
     if file_name in os.listdir(os.curdir):
-        if name[:-1].endswith("copy"):
-            copy_num = int(name[-1]) + 1
-            return "{}{}.{}".format(name[:-1], copy_num, ext)
+        print('1')
+        if name[:-1].endswith('copy'):
+            print(2)
+            copy_n = int(name[-1]) + 1
+            result = "{}{}.{}".format(name[:-1], copy_n, ext)
         else:
-            return "{}_copy1.{}".format(name, ext)
+            print(3)
+            result = "{}_copy1.{}".format(name, ext)
+            if result in os.listdir(os.curdir):
+                return name_manager(result, dec=False)
     else:
-        return file_name
+        print(4)
+        result = file_name
+    print('received file: %s' % result)
+    return bytes(result, "utf-8")
 
 
 def recv_file():
+    print("hosted on port %i ...." % PORT)
+    s = socket.socket()
+    s.bind(("localhost", PORT))
+    s.listen(3)
     while True:
         client_socket, address = s.accept()
+        print("accepted new connection: ", address)
         file_name = client_socket.recv(32)
+        if not file_name:
+            continue
         f = open(name_manager(file_name), 'wb')
         while True:
             p = client_socket.recv(1024)
             while p:
                     f.write(p)
-                    p = sc.recv(1024)
+                    p = client_socket.recv(1024)
         f.close()
-
 
         client_socket.close()
 
--- lab6/send_file.py
+++ lab6/send_file.py
@@ -1,14 +1,18 @@
+from tqdm import tqdm
 import argparse
 import socket
+import os
 
 
 def send_file(file_name, host_name, port):
     file = open(file_name, "rb")
     s = socket.socket()
     s.connect((host_name, port))
-    s.send(file_name)
+    s.send(bytes(file_name + ("|" * (32 - len(file_name))), "utf-8"))
     p = file.read(1024)
-    while p:
+
+    size = os.path.getsize(file_name)
+    for _ in tqdm(range((size // 1024) + 1)):
         s.send(p)
         p = file.read(1024)
     s.close()
@@ -21,4 +25,4 @@ if __name__ == '__main__':
     parser.add_argument("host_name", type=str)
     parser.add_argument("port", type=int)
     args = parser.parse_args()
-    send_file(parser.file_name, parser.host_name, parser.port)
+    send_file(args.file_name, args.host_name, args.port)
