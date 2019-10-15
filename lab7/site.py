823acf746ae11d07e354df545f7fb5c4495cba6b
Danil Ginzburg <danilgin@inbox.ru>
2019-10-15 23:10:25 +0300

    added docker-compose config files

--- lab7/site.py
+++ lab7/site.py
@@ -1,12 +1,17 @@
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 host = '0.0.0.0'
 
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>lab7 here!</h1>"
+def index():
+    return render_template('index.html')
+
+
+@app.route("/page2")
+def page2():
+    return render_template('page2.html')
 
 
 if __name__ == "__main__":
