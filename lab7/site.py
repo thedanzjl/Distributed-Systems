ec780456dfca679e15f0d4200ce5f799ee3d454b
Danil Ginzburg <danilgin@inbox.ru>
2019-10-17 21:14:48 +0300

    added screenshots into htmls

--- lab7/site.py
+++ lab7/site.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 app = Flask(__name__)
 host = '0.0.0.0'
 
@@ -12,6 +12,16 @@ def index():
 @app.route("/page2")
 def page2():
     return render_template('page2.html')
+
+#
+# @app.route('/cloudflare.jpg')
+# def image1():
+#     return send_file('cloudflare.jpg', mimetype='image/jpg')
+#
+#
+# @app.route('/nginx_conf.jpg')
+# def image2():
+#     return send_file('nginx_conf.jpg', mimetype='image/jpg')
 
 
 if __name__ == "__main__":
