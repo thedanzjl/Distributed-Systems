582ca75892a8ec16da3018ac0ed78737c3cff4db
Danil Ginzburg <danilgin@inbox.ru>
2019-10-24 15:11:01 +0300

    init commit

---
+++ lab8/logical_clock.py
@@ -0,0 +1,91 @@
+from multiprocessing import Process, Pipe
+
+
+def local_time(counter):
+    return ' LAMPORT_TIME={}'.format(counter)
+
+
+def calc_recv_timestamp(recv_time_stamp, counter):
+    for id in range(len(counter)):
+        counter[id] = max(recv_time_stamp[id], counter[id])
+    return counter
+
+
+def event(pid, counter):
+    counter[pid] += 1
+    print('Something happened in {} !'.\
+          format(pid) + local_time(counter))
+    return counter
+
+
+def send_message(pipe, pid, counter):
+    counter[pid] += 1
+    pipe.send(('Empty shell', counter))
+    print('Message sent from ' + str(pid) + local_time(counter))
+    return counter
+
+
+def recv_message(pipe, pid, counter):
+    message, timestamp = pipe.recv()
+    counter = calc_recv_timestamp(timestamp, counter)
+    counter[pid] += 1
+    print('Message received at ' + str(pid) + local_time(counter))
+    return counter
+
+
+def process_a(pipe12):
+    counter = [0, 0, 0]
+    pid = 0
+    counter = send_message(pipe12, pid, counter)
+    counter = send_message(pipe12, pid, counter)
+    counter = event(pid, counter)
+    counter = recv_message(pipe12, pid, counter)
+    counter = event(pid, counter)
+    counter = event(pid, counter)
+    counter = recv_message(pipe12, pid, counter)
+    print('process_a', counter)
+
+
+def process_b(pipe21, pipe23):
+    counter = [0, 0, 0]
+    pid = 1
+    counter = recv_message(pipe21, pid, counter)
+    counter = recv_message(pipe21, pid, counter)
+    counter = send_message(pipe21, pid, counter)
+    counter = recv_message(pipe23, pid, counter)
+    counter = event(pid, counter)
+    counter = send_message(pipe21, pid, counter)
+    counter = send_message(pipe23, pid, counter)
+    counter = send_message(pipe23, pid, counter)
+    print('process_b', counter)
+
+
+def process_c(pipe32):
+    counter = [0, 0, 0]
+    pid = 2
+    counter = send_message(pipe32, pid, counter)
+    counter = recv_message(pipe32, pid, counter)
+    counter = event(pid, counter)
+    counter = recv_message(pipe32, pid, counter)
+    print('process_c', counter)
+
+
+if __name__ == '__main__':
+    oneandtwo, twoandone = Pipe()
+    twoandthree, threeandtwo = Pipe()
+
+    process1 = Process(target=process_a,
+                       args=(oneandtwo,))
+    process2 = Process(target=process_b,
+                       args=(twoandone, twoandthree))
+    process3 = Process(target=process_c,
+                       args=(threeandtwo,))
+
+    process1.start()
+    process2.start()
+    process3.start()
+
+    process1.join()
+    process2.join()
+    process3.join()
+
